6d33efd2728583340f17a5273c6f35f6e51f9431
crystal_b <crystal.beecken@gmail.com>
2022-05-31 18:05:08 +0900

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var fans = document.querySelector(".fans");
+var fish = document.querySelector(".fish");
+var pets = document.querySelector(".pets");
+var themeToggle = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
+
+fans.addEventListener("mouseover", function () {
+  fans.innerText = "123K";
+});
+fish.addEventListener("mouseover", function () {
+  fish.innerText = "47k";
+});
+pets.addEventListener("mouseover", function () {
+  pets.innerText = "20k";
+});
+
+themeToggle.addEventListener("click", function () {
+  if (body.classList.contains("light")) {
+    body.classList.remove("light");
+    ball.classList.remove("move-right");
+    h1.innerText = "Stealth Quincy";
+  } else {
+    body.classList.add("light");
+    ball.classList.add("move-right");
+    h1.innerText = "Party Quincy";
+  }
+});
